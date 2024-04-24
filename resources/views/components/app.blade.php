635de1d76c126453d122cedca5514372c299bfa3
Mohammad <mohammadkh12221@gmail.com>
2024-04-24 21:37:34 +0330

    implemented dashboard page

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -1,3 +1,17 @@
-<div>
-    <!-- The only way to do great work is to love what you do. - Steve Jobs -->
-</div>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite("resources/css/app.css")
+    @stack("css")
+    <title>{{ $title ?? "Title" }}</title>
+</head>
+<body>
+    {{ $slot }}
+
+@stack("js")
+</body>
+</html>
